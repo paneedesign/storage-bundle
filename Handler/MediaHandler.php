4d7a8e91a67a1e5c26cb938f2a677d4e0cc42c17
luigi <cardamoneluigi@gmail.com>
2018-06-11 15:28:38 +0200

    Fix FileType description

--- Handler/MediaHandler.php
+++ Handler/MediaHandler.php
@@ -55,7 +55,7 @@ class MediaHandler
     private $key;
 
     /**
-     * Media Type (es. profile, gallery, document, thumbnail)
+     * File Type (es. image, video, document)
      *
      * @var string
      */
